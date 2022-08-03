811f6a49d4f52531b351a46f4dc32edacef4db86
Manuela Sabatino <mn.sabatino@gmail.com>
2022-08-03 14:53:57 +0100

    Add the map marker but now the popup is missing

--- what3words/widgets/what3words/src/runtime/widget.tsx
+++ what3words/widgets/what3words/src/runtime/widget.tsx
@@ -2,9 +2,11 @@
 import { React, AllWidgetProps, jsx } from 'jimu-core'
 import { JimuMapViewComponent, JimuMapView } from 'jimu-arcgis'
 import Point from 'esri/geometry/Point'
-import { getCurrentAddress } from './addlocator'
+import { getCurrentAddress, getMarkerGraphic } from './addlocator'
 
 export default class Widget extends React.PureComponent<AllWidgetProps<any>, any> {
+  mapView: any
+
   constructor (props) {
     super(props)
     const geocodeServiceURL = this.props.config.w3wLocator
@@ -24,25 +26,45 @@ export default class Widget extends React.PureComponent<AllWidgetProps<any>, any
 
   activeViewChangeHandler = (jmv: JimuMapView) => {
     if (jmv) {
+      this.mapView = jmv.view
       this.setState({
         jimuMapView: jmv
       })
       this.setState({
         w3wLocator: this.props.config.w3wLocator
       })
-      jmv.view.on('click', (mapClick) => {
+      this.mapView.on('click', async (mapClick) => {
+        this.mapView.graphics.removeAll()
+        const graphic = await getMarkerGraphic(mapClick.mapPoint)
+        const latitude = Math.round(mapClick.mapPoint.latitude * 1000) / 1000
+        const longitude = Math.round(mapClick.mapPoint.longitude * 1000) / 1000
         const point: Point = this.state.jimuMapView.view.toMap({
-          x: mapClick.x,
-          y: mapClick.y
+          x: longitude,
+          y: latitude
         })
         this.setState({
           latitude: point.latitude.toFixed(4),
           longitude: point.longitude.toFixed(4)
         })
-        getCurrentAddress(this.state.w3wLocator, mapClick.mapPoint).then(res => {
+        this.mapView.popup.open({
+          // Set the popup's title to the coordinates of the location
+          title: 'Reverse geocode: [' + longitude + ', ' + latitude + ']',
+          location: mapClick.mapPoint // Set the location of the popup to the clicked location
+        })
+        console.log(this.mapView.popup.title)
+
+        getCurrentAddress(this.state.w3wLocator, mapClick.mapPoint).then(response => {
           this.setState({
-            what3words: res
+            what3words: response
           })
+          this.mapView.popup.content = 'what3words address: ///' + response
+          console.log(this.mapView.popup.content)
+          this.mapView.graphics.add(graphic)
+        }).catch((error) => {
+          console.log('error: ' + error)
+          // If the promise fails and no result is found, show a generic message
+          this.mapView.popup.content = 'No address was found for this location'
+          this.mapView.graphics.removeAll()
         })
       })
     }
