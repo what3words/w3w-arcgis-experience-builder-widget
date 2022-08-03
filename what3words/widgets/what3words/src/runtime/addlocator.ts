fe467a33bbaf3369fb82bd09d01a86258f6f7619
Manuela Sabatino <mn.sabatino@gmail.com>
2022-08-03 20:47:18 +0100

    Add the map marker and the popup

--- what3words/widgets/what3words/src/runtime/addlocator.ts
+++ what3words/widgets/what3words/src/runtime/addlocator.ts
@@ -3,7 +3,6 @@ import { loadArcGISJSAPIModules } from 'jimu-arcgis'
 const w3wIcon = require('../assets/w3wMarker.png')
 
 export const getCurrentAddress = (geocodeURL: string, position: Point) => {
-  // const position = getCurrentLocation()
   if (!position) return Promise.resolve(null)
   return loadArcGISJSAPIModules(['esri/rest/locator']).then(modules => {
     const [locator] = modules
@@ -34,38 +33,6 @@ export const getMarkerGraphic = (position: Point) => {
     return new Graphic({
       geometry: position,
       symbol: symbol
-    })
-  })
-}
-
-/**
- * Get current location
-*/
-export const getCurrentLocation = (onSeccess: (position) => void, onError) => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(onSeccess, onError)
-  } else {
-    onError && onError()
-  }
-}
-
-/**
- * Create point by position
-*/
-export const createPoint = (position: GeolocationPosition): Promise<__esri.Point> => {
-  const coords = position && position.coords
-  if (!coords) {
-    return Promise.resolve(null)
-  }
-  return loadArcGISJSAPIModules(['esri/geometry/Point']).then(modules => {
-    const [Point] = modules
-    return new Point({
-      longitude: coords.longitude,
-      latitude: coords.latitude,
-      z: coords.altitude || null,
-      spatialReference: {
-        wkid: 4326
-      }
     })
   })
 }
